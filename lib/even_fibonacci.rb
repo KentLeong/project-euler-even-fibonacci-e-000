d7c244dec4d104ae71dc7c3d7871a47d9e7ce13d
KentLeong <erickentleongchu@gmail.com>
2015-11-24 15:41:16 +0000

    nitrous-backup at Tue Nov 24 15:41:16 UTC 2015

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,10 +1,14 @@
 # Implement your procedural solution here!
 def even_fibonacci_sum(number)
-a = 0
-b = 1
+a = 1
+b = 2
 c = 0
-  
-until i == number do 
+i = 2
+until i == number do
   i = i + 1
-  
+  c = a + b
+  a = b
+  b = c
+end
+return c
 end
